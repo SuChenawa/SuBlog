40fca0f6be5d9fcff1ebbc2f8af03bdc802f22d7
SuChenawa <wjl1303121277@gmail.com>
2022-12-01 20:39:47 +0800

    auto update

--- SuricPlugins/file.py
+++ SuricPlugins/file.py
@@ -1,4 +1,5 @@
 import os
+import json
 def rename(path):
     file_list=os.listdir(path)
     for i in range(0,10):
@@ -14,10 +15,30 @@ def rename(path):
             else:
                 print("Sucess!")
 
+
+
 if __name__=="__main__":
 
     path="./fantaspic/"
     rename(path)
     path="./husband/"
     rename(path)
-       
+
+    fantaspicpath = './fantaspic/'      # 输入文件夹地址
+    fantaspicfiles = os.listdir(fantaspicpath)   # 读入文件夹
+    num_png = len(fantaspicfiles)       # 统计文件夹中的文件个数
+    fantaspic_dict = {'picNum':num_png}
+    fantaspic_json_str = json.dumps(fantaspic_dict)
+    print(fantaspic_json_str)
+    with open("./fantaspics.json","w") as file:
+        json.dump(fantaspic_dict,file)
+        print("写入成功")
+    husbandpath = './husband/'      # 输入文件夹地址
+    husbandfiles = os.listdir(husbandpath)   # 读入文件夹
+    num_png = len(husbandfiles)       # 统计文件夹中的文件个数
+    husband_dict = {'picNum':num_png}
+    husband_json_str = json.dumps(husband_dict)
+    print(husband_json_str)
+    with open("./husbands.json","w") as file:
+        json.dump(husband_dict,file)
+        print("写入成功")
